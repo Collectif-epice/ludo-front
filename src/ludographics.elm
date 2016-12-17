cdaac9230a7f8390f528bb91b343e2792238edcc
Anthony Rey <anthony.rey5@gmail.com>
2016-12-17 18:10:21 +0100

    some fixes and started to draw the board

--- src/ludographics.elm
+++ src/ludographics.elm
@@ -5,26 +5,48 @@ import Color exposing (..)
 import Element exposing (..)
 import Collage exposing (..)
 
-type alias Square = {x : Float, y : Float, aColor : Color }
+type alias Square = {x : Float, y : Float, size : Float, aColor : Color }
 
 generateBoard : Html.Html msg
 generateBoard =
     let
-       board = [ {x = 30, y = 30, aColor = blue},
-                 {x = 60, y = 60, aColor = red },
-                 {x = 60, y = 35, aColor = green} ]
+       board = [ {x = 0, y = 0, size = 10, aColor = red},
+                 {x = 1, y = 0, size = 10, aColor = red},
+                 {x = 2, y = 0, size = 10, aColor = red},
+                 {x = 3, y = 0, size = 10, aColor = red},
+                 {x = 4, y = 0, size = 10, aColor = red},
+                 {x = 5, y = 0, size = 10, aColor = red},
+                 {x = 6, y = 0, size = 10, aColor = red},
+                 {x = 7, y = 0, size = 10, aColor = red},
+
+                 {x = 7, y = -1, size = 10, aColor = red},
+                 {x = 7, y = -2, size = 10, aColor = red},
+                 {x = 7, y = -3, size = 10, aColor = red},
+                 {x = 7, y = -4, size = 10, aColor = red},
+                 {x = 7, y = -5, size = 10, aColor = red},
+                 {x = 7, y = -6, size = 10, aColor = red} ]
 
        generateForms : List Square -> List Form
        generateForms board =
             case board of
                 []         -> []
-                head::tail -> (move (head.x, head.y) (makeCircle head.aColor)) :: (generateForms tail)
+                head::tail -> (makeCircle head.aColor head.x head.y head.size) :: (generateForms tail)
 
        generateElement : List Form -> Element
        generateElement forms =
             collage 500 500 forms
+
+       makeCircle : Color -> Float -> Float -> Float -> Form
+       makeCircle color x y size =
+         let
+           padding = 2
+           realSize = size * 2
+
+           toRealPosition : Float -> Float
+           toRealPosition x =
+                (x * (realSize + padding))
+         in
+           (move (toRealPosition x, toRealPosition y) (filled color (circle size)))
+
     in
         toHtml (generateElement (generateForms board))
-
-makeCircle color =
-  filled color (circle 5)
